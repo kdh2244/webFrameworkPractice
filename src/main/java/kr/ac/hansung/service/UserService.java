3a83f54338acad8029788dca785e1153465cca74
kdh <ihel@naver.com>
2026-05-31 17:46:32 +0900

    feat/UserController changePassword 요청 추가, UserSerivce changePassword 메소드 추가

--- src/main/java/kr/ac/hansung/service/UserService.java
+++ src/main/java/kr/ac/hansung/service/UserService.java
@@ -1,14 +1,23 @@
 package kr.ac.hansung.service;
 
+import jakarta.validation.Valid;
+import kr.ac.hansung.dto.PasswordChangeDto;
 import kr.ac.hansung.dto.UserDto;
 import kr.ac.hansung.entity.Role;
 import kr.ac.hansung.entity.User;
 import kr.ac.hansung.repository.RoleRepository;
 import kr.ac.hansung.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Service
 @RequiredArgsConstructor
@@ -32,6 +41,18 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(dto.getPassword()));
         user.getRoles().add(userRole);
 
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void changePassword(String email, String currentPassword, String newPassword){
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+
+        if (!passwordEncoder.matches(currentPassword,user.getPassword())){
+            throw new IllegalArgumentException("현재 비밀버호가 일치하지 않습니다.");
+        }
+        user.setPassword(passwordEncoder.encode(newPassword));
         userRepository.save(user);
     }
 
